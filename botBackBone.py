fd504debbf4067854e9b98bcf529f50fa29214c5
Noraldim bakhit <58360899+Noraldim@users.noreply.github.com>
2023-02-12 16:10:27 +0300

    Add files via upload

---
+++ botBackBone.py
@@ -0,0 +1,63 @@
+from typing import Counter
+
+import requests 
+import pandas as pd
+resp = ["ما فهمتك","معليش بحاول أتعلم في السوداني ","Sorry i didn understand you ","have a nice day"]
+dburl = "https://raw.githubusercontent.com/Noraldim/RESOURCES/master/FULL%20BOT%20RESORSEs.tsv"
+url = "https://api.telegram.org/bot5877922080:AAF3bU4om-Yoksl39wZyLR_hvxn87vAqeQw/"
+counter = 0
+# download the tsv file from the link and save it locally
+df = pd.read_csv(dburl, sep = "\t")
+
+def read(offset):
+
+    para = {
+            "offset" : offset          
+        }
+
+    req = requests.get(url + "getUpdates", data = para)
+    data = req.json()
+    print(data)
+
+    for result in data["result"]:
+        send(result)
+
+    if data["result"]:
+        return data["result"][-1]["update_id"] + 1
+
+def out(message):
+  global counter
+  answer = df.loc[df['qus'].str.lower()== message.lower()]
+
+
+  if not answer.empty:
+    answer = answer.iloc[0]['ans']
+    return answer
+  else:
+    counter = (counter + 1) % len(resp)
+    return resp[counter]
+    
+              
+ 
+
+def send(result):
+
+  text = result["message"]["text"]
+  answer = out(text)
+
+
+  parameter = {
+      
+      "chat_id" : result["message"]["chat"]["id"],        
+      "text" : answer  ,
+      "reply_to_message_id" : None     
+  }
+
+
+  req = requests.get(url + "sendMessage", data = parameter)
+  print(req.text)
+
+offset = 0
+
+while True :
+  offset = read(offset)
